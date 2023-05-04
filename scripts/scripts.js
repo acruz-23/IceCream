2067502036b2c9bb032d7164e3a8f8c07dcfc261
acruz-23 <cruz.581@outlook.com>
2023-05-04 10:48:44 -0400

    form now calculates the total cost, taking into account number of scoops, toppings, and taxes.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,30 +1,15 @@
 console.log("JS working");
-// const inputEl = document.querySelector(
-//   "input[class='disableAtStart']:disabled"
-// );
-// inputEl.style.display = "none";
-// const dayEl = document.getElementById("numOfDays");
-// const pickupDateEl = document.getElementById("pickupDate");
+
 const submitOrderBtnEl = document.getElementById("submitOrderBtn");
 // const over25ChargeEl = document.getElementById("noRadioBtn");
 // const under25surcharge = document.getElementById("yesRadioBtn");
 // over25ChargeEl.onclick = onageSelectorElChecked;
 // under25surcharge.onclick = onageSelectorElChecked;
 submitOrderBtnEl.onclick = onsubmitOrderBtnClicked;
-// dayEl.onchange = onChangeEl;
-// pickupDateEl.onchange = onChangeEl;
-
-// function onChangeEl() {
-//   console.log("onChangeEl");
-
-//   if (dayEl.value != "" && pickupDateEl != "") {
-//     inputEl.enabled;
-//   }
-//   console.log("onChangeEl");
-// }
 
 function onageSelectorElChecked() {
   console.log("onageSelectorElChecked Started");
+  const toppingOptions = document.getElementById("toppingOptions");
   const surchageRow = document.getElementById("surchargeRow");
   if (under25surcharge.checked) {
     surchageRow.style.display = "table-row";
@@ -38,15 +23,39 @@ function onsubmitOrderBtnClicked() {
   console.log("onsubmitOrderBtnClicked Started");
   // calc base ice cream cost
   const numOfScoops = +document.getElementById("numOfScoops").value;
-  let basePrice = 2.25 + (numOfScoops - 1) * 1.25;
+  let scoopPrice = 2.25 + (numOfScoops - 1) * 1.25;
+  console.log("num of scoops ", numOfScoops);
+  console.log("scoop price", scoopPrice);
 
   //calc Topping Total
+  const sprinkleOptionEl = document.getElementById("sprinkleOption");
+  const whippedCreamOptionEl = document.getElementById("whippedCreamOption");
+  const hotFudgeOptionEl = document.getElementById("hotFudgeOption");
+  const cherryOptionEl = document.getElementById("cherryOption");
+  let toppingsPrice = 0;
+  if (sprinkleOptionEl.checked) {
+    toppingsPrice += 0.5;
+  }
+  if (whippedCreamOptionEl.checked) {
+    toppingsPrice += 0.25;
+  }
+  if (hotFudgeOptionEl.checked) {
+    toppingsPrice += 1.25;
+  }
+  if (cherryOptionEl.checked) {
+    toppingsPrice += 0.25;
+  }
 
   //calc base price
+  const basePrice = scoopPrice + toppingsPrice;
   document.getElementById("basePrice").innerHTML = " " + basePrice.toFixed(2);
 
   //calc tax 6% for my area
-  console.log("onsubmitOrderBtnClicked finished");
+  const taxAdded = basePrice * 0.06;
+  document.getElementById("taxAdded").innerHTML = " " + taxAdded.toFixed(2);
 
   //calc Total Due
+  const totalDue = basePrice + taxAdded;
+  document.getElementById("totalDue").innerHTML = " " + totalDue.toFixed(2);
+  console.log("onsubmitOrderBtnClicked finished");
 }
